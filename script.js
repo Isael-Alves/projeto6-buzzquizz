8f172c1426c3f7558cea594fc1a6d87fb1efee62
= <isael.alves@tucurui.ufpa.br>
2022-06-30 03:17:04 -0300

    Function embarralhar cards da tela dois ok

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let ArrayOptions = [];
 
 function functionsIniciais() {
     IniciandoBuzzQuizz();
@@ -38,14 +39,20 @@ function EntrandoQuizz(quizz) {
         const DadosQuizz = resposta.data;
         const Questions = DadosQuizz.questions;
 
-        console.log(Questions);
         document.querySelector(".TitleQuizz").innerHTML += `
            <img src=${DadosQuizz.image} alt="">
            <h2>${DadosQuizz.title}</h2>
         `
-
         Questions.map((dados, i) => {
-            console.log(dados);
+            let Option = dados.answers;
+
+            shuffleArray([0,1,1,1]);
+            const Card1 = Option[ArrayOptions[0]];
+            const Card2 = Option[ArrayOptions[1]];
+            const Card3 = Option[ArrayOptions[2]];
+            const Card4 = Option[ArrayOptions[3]];
+
+            console.log(dados.answers);
             document.querySelector(".BoxQuestions").innerHTML += `
             <article class="Questions">
                     <div class="Question">
@@ -53,28 +60,39 @@ function EntrandoQuizz(quizz) {
                     </div>
 
                     <ul>
-                        <li>
-                            <img src="img/image 3.svg" alt="">
-                            <h5>Gatíneo</h5>
+                        <li class="${Card1.isCorrectAnswer}">
+                            <img src="${Card1.image}" alt="">
+                            <h5>${Card1.text}</h5>
                         </li>
-                        <li>
-                            <img src="img/image 3.svg" alt="">
-                            <h5>Gatíneo</h5>
+                        <li class="${Card2.isCorrectAnswer}">
+                            <img src="${Card2.image}" alt="">
+                            <h5>${Card2.text}</h5>
                         </li>
-                        <li>
-                            <img src="img/image 3.svg" alt="">
-                            <h5>Gatíneo</h5>
+                        <li class="${Card3.isCorrectAnswer}">
+                            <img src="${Card3.image}" alt="">
+                            <h5>${Card3.text}</h5>
                         </li>
-                        <li>
-                            <img src="img/image 3.svg" alt="">
-                            <h5>Gatíneo</h5>
+                        <li class="${Card4.isCorrectAnswer}">
+                            <img src="${Card4.image}" alt="">
+                            <h5>${Card4.text}</h5>
                         </li>
                     </ul>
             </article>`
 
         });
         FinishingQuizz();
-    });   
+
+    });
+}
+
+//Embarralhador de Arrays
+function shuffleArray(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    ArrayOptions = arr;
+    return ArrayOptions;
 }
 
 function FinishingQuizz() {
